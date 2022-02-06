af99f07fb0bc5ae8e95fa7a63e958bbc968e15e0
trungne <nguyenquochoangtrung@gmail.com>
2022-02-07 00:47:27 +0700

    add button in front of img

--- src/non-tech-portfolio/NonTechPortfolio.tsx
+++ src/non-tech-portfolio/NonTechPortfolio.tsx
@@ -1,9 +1,12 @@
 import styles from "./nontech.module.css";
 import globalStyles from "../global.module.css";
 import Typography from "@mui/material/Typography";
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 import classroomImg from "./static/classroom.jpg";
 import videoImg from "./static/naune.jpg";
+import { useState } from "react";
 
 export default function NonTechPortfolio() {
     return (
@@ -21,7 +24,7 @@ export default function NonTechPortfolio() {
 function Header() {
     return (
         <div className={styles["header"] + " " + globalStyles["center-flex"]}>
-            <Typography variant="h2" className={globalStyles["white-text"]}>
+            <Typography variant="h2" className={globalStyles["white-text"] + " " + styles["header-text"]}>
                 Besides coding, I also enjoy teaching English and making informative videos
             </Typography>
         </div>
@@ -29,12 +32,39 @@ function Header() {
 }
 
 function Image(props: { imgPath: string, caption: string }) {
+    const [isHovering, setIsHovering] = useState(false);
+    const hoveringStyle = {
+        opacity: 0.5,
+    }
+    const nonHoveringStyle = {
+        opacity: 1,
+    }
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    }
+
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    }
+
     return (
         <div className={styles["image"]}>
-            <img alt={props.caption} src={props.imgPath} style={{width: "100%"}}/>
+            <div onMouseOver={handleMouseOver} onMouseLeave={handleMouseOut} className={styles["image-container"]} style={isHovering ? hoveringStyle : nonHoveringStyle}>
+                <img alt={props.caption} src={props.imgPath} style={{ width: "100%" }} />
+                {isHovering && <FloatingButton url={""} />}
+            </div>
+
             <Typography className={styles["caption"] + " " + globalStyles["quote"]} variant="caption">
                 {props.caption}
             </Typography>
         </div>
+    )
+}
+
+function FloatingButton(props: { url: string }) {
+    return (
+        <Fab className={styles["button"]} color="primary" aria-label="add">
+            <AddIcon />
+        </Fab>
     )
 }
