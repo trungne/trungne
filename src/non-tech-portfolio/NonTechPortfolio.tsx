0c73fe204cd83e988062c6f8586f0f1fe90caa50
trungne <nguyenquochoangtrung@gmail.com>
2022-02-06 18:30:17 +0700

    fix width overflow

--- src/non-tech-portfolio/NonTechPortfolio.tsx
+++ src/non-tech-portfolio/NonTechPortfolio.tsx
@@ -1,8 +1,20 @@
-import styles from "./nontech.module.css"
-export default function NonTechPortfolio(){
+import styles from "./nontech.module.css";
+import globalStyles from "../global.module.css";
+import Typography from "@mui/material/Typography";
+export default function NonTechPortfolio() {
     return (
-        <div id="non-tech" className={styles.nontech}>
+        <div id="non-tech" className={styles["non-tech"]}>
+            <Header />
+        </div>
+    )
+}
 
+function Header() {
+    return (
+        <div className={styles["header"] + " " + globalStyles["center-flex"]}>
+            <Typography variant="h6" className={globalStyles["white-text"]}>
+                Besides coding, I enjoy teaching English and making informative videos
+            </Typography>
         </div>
     )
 }
