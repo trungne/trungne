d70b1835dda3483a9b8a615a21114bc2d5a462c8
trungne <nguyenquochoangtrung@gmail.com>
2022-02-11 02:23:00 +0700

    add more contact info

--- src/contact/Info.tsx
+++ src/contact/Info.tsx
@@ -1,4 +1,6 @@
 import Typography from "@mui/material/Typography";
+import Paper from '@mui/material/Paper';
+import Tooltip from '@mui/material/Tooltip';
 
 import globalStyles from "../global.module.css";
 import styles from "./contact.module.css";
@@ -6,22 +8,64 @@ import githubIcon from "./static/github.png";
 import gmailIcon from './static/gmail.png';
 import zaloIcon from './static/zalo.png';
 
-
+import { useState } from "react";
 
 export default function Info() {
+    const [info, setInfo] = useState<InfoCardProps>({ info: "" });
+
     return (
         <div className={`${globalStyles['center-flex']} ${styles['info']}`}>
-            <Typography variant="h4" className={`${globalStyles["white-text"]} ${styles['text']}`}>
-                Find me at
-            </Typography>
+            <div className={styles['find-me']}>
+                <Typography variant="h4" className={`${globalStyles["white-text"]} `}>
+                    Find me at
+                </Typography>
+
+                {info && <InfoCard info={info.info} link={info.link} />}
+            </div>
+
             <div className={styles['icon-container']}>
                 {/* <a href="https://github.com/trungne" rel="noreferrer" target="_blank"> */}
-                <img className={styles['icon']} alt="github icon" src={githubIcon} />
-                <img className={styles['icon']} alt="gmail icon" src={gmailIcon} />
-                <img className={styles['icon']} alt="zalo icon" src={zaloIcon} />
+                <img onClick={() => { setInfo({ info: "GitHub", link: "https://github.com/trungne" }) }} className={styles['icon']} alt="github icon" src={githubIcon} />
+                <img onClick={() => { setInfo({ info: "nguyenquochoangtrung@gmail.com", link: "" }) }} className={styles['icon']} alt="gmail icon" src={gmailIcon} />
+                <img onClick={() => { setInfo({ info: "+84 93 919 8601", link: "" }) }} className={styles['icon']} alt="zalo icon" src={zaloIcon} />
             </div>
 
 
         </div>
+    )
+}
+interface InfoCardProps {
+    info: string,
+    link?: string,
+
+}
+function InfoCard({ info, link }: InfoCardProps) {
+    const clickToSee = (
+        <span style={{fontStyle: "italic !important"}}>
+            Click to reveal
+        </span>
+    )
+    const handleClick = () => {
+        if (link) {
+            Object.assign(document.createElement('a'), {
+                target: '_blank',
+                href: link,
+            }).click();
+        }
+        else {
+            navigator.clipboard.writeText(info.trim().replace(/\s/g, ''))
+        }
+    }
+    return (
+        <Tooltip disableHoverListener={!info} placement='left-start' title={link ? "Click to redirect" : "Click to copy" }>
+            <Paper className={styles['paper']} elevation={3}>
+                <Typography variant="caption" className={`${styles['text']}`}>
+                    <div onClick={handleClick}>
+                        {info ? info : clickToSee}
+                    </div>
+                </Typography>
+            </Paper>
+        </Tooltip>
+
     )
 }
